6900fd6f62e35523290785eff6ea314d13d267a4
Ji Hua <michaelhua1993@foxmail.com>
2021-03-31 23:31:14 -0400

    add new sort methods

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -19,9 +19,10 @@ def bubble_sort(nums: List[int]) -> List:
     for i in range(len(nums)):
         # NOTE: it will carry the largest in the unsorted 
         # to the end of the list.
-        for j in range(len(nums)-1-i):
-            if nums[j] > nums[j+1]:
-                nums[j], nums[j+1] = nums[j+1], nums[j]
+        j_range = len(nums) - 1 - i
+        for j in range(j_range):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
     return nums
 
@@ -43,7 +44,7 @@ def selection_sort(nums: List[int]) -> List:
 
     for i in range(len(nums)):
         min_index = i
-        for j in range(i+1, len(nums)):
+        for j in range(i + 1, len(nums)):
             if nums[j] < nums[min_index]:
                 min_index = j
         nums[i], nums[min_index] = nums[min_index], nums[i]
@@ -73,6 +74,154 @@ def insertion_sort(nums: List[int]) -> List:
                 nums.insert(j, val)
                 break
         else:
-            nums.insert(i+1, val)
+            nums.insert(i + 1, val)
 
     return nums
+
+
+def shell_sort(nums: List[int]) -> List:
+    """ Use shell sort algorithm to sort
+
+        Input:
+            nums: List of integers
+
+        Return:
+            List of integers
+    """
+    if not isinstance(nums, list):
+        raise ValueError(f"Invalid input {nums}")
+
+    if nums == []:
+        return nums
+
+    gap = len(nums)
+    while gap > 1:
+        gap = gap // 2
+        for i in range(gap, len(nums)):
+            val = nums[i]
+            j = i
+            while j > 0 and nums[j - gap] > val:
+                nums[j] = nums[j - gap]
+                j -= gap
+            nums[j] = val
+
+    return nums
+
+
+def merge_sort(nums: List[int]) -> List:
+    """ Use merge sort algorithm to sort
+
+        Input:
+            nums: List of integers
+
+        Return:
+            List of integers
+    """
+    if not isinstance(nums, list):
+        raise ValueError(f"Invalid input {nums}")
+
+    if nums == []:
+        return nums
+
+    if len(nums) == 1:
+        return nums
+
+    def merge(left, right):
+        results = []
+        while left and right:
+            if left[0] <= right[0]:
+                results.append(left.pop(0))
+            else:
+                results.append(right.pop(0))
+        if left:
+            results.extend(left)
+        else:
+            results.extend(right)
+        return results
+
+    middle = len(nums) // 2
+    left, right = nums[:middle], nums[middle:]
+
+    return merge(merge_sort(left), merge_sort(right))
+
+
+def merge_sort_iteration(nums: List[int]) -> List:
+    """ Use merge sort by iteration algorithm to sort
+
+        Input:
+            nums: List of integers
+
+        Return:
+            List of integers
+    """
+    if not isinstance(nums, list):
+        raise ValueError(f"Invalid input {nums}")
+
+    if nums == []:
+        return nums
+
+    def merge(left, right):
+        results = []
+        while left and right:
+            if left[0] <= right[0]:
+                results.append(left.pop(0))
+            else:
+                results.append(right.pop(0))
+        if left:
+            results.extend(left)
+        else:
+            results.extend(right)
+        return results
+
+    batch_size = 1
+    while batch_size < len(nums):
+        start = 0
+        while start < len(nums):
+            middle = start + batch_size
+            end = middle + batch_size
+            left, right = nums[start:middle], nums[middle:end]
+            nums[start:end] = merge(left, right)
+            start = end
+        batch_size *= 2
+
+    return nums
+
+
+# FIXME: this is still not perfect, it will fail
+#  with [0] * 1000 test case
+# RecursionError: maximum recursion depth exceeded in comparison
+def quick_sort(nums: List[int]) -> List:
+    """ Use quick sort algorithm to sort
+
+        Input:
+            nums: List of integers
+
+        Return:
+            List of integers
+    """
+    if not isinstance(nums, list):
+        raise ValueError(f"Invalid input {nums}")
+
+    if nums == []:
+        return nums
+
+    def _sort(nums, left, right):
+        if left >= right:
+            return nums
+        
+        pivot = left
+        val = nums[pivot]
+        i, j = left, right
+        while i < j:
+            while i < j and nums[j] >= val:
+                j -= 1
+            while i < j and nums[i] <= val:
+                i += 1
+            if i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i], nums[pivot] = nums[pivot], nums[i]
+        nums = _sort(nums, left, i - 1)
+        nums = _sort(nums, i + 1, right)
+        return nums
+
+    return _sort(nums, 0, len(nums) - 1)
--- test/test_sort.py
+++ test/test_sort.py
@@ -1,8 +1,9 @@
 import unittest
 import random
 
-from algorithm.sort import (bubble_sort,
-    selection_sort, insertion_sort)
+from algorithm.sort import (bubble_sort, selection_sort,
+    insertion_sort, shell_sort, merge_sort, merge_sort_iteration,
+    quick_sort)
 
 
 large_nums = list(range(10000))
@@ -15,34 +16,39 @@ TESTCASES = [
     [6,2,3,4,1,2,3,1,7],
     list(range(20, 0, -1)),
     large_nums,
-    [0] * 1000,
-    [-1, 1] * 30,
+    [0] * 100,
+    [-1, 1] * 100,
 ]
 
 class SortTestCase(unittest.TestCase):
     def test_bubble_sort(self):
-        with self.assertRaises(ValueError):
-            bubble_sort(None)
-            bubble_sort('1,2,3')
-
-        for test_case in TESTCASES:
-            self.assertEqual(bubble_sort(test_case), sorted(test_case))
+        self._test_sort(bubble_sort)
 
     def test_selection_sort(self):
-        with self.assertRaises(ValueError):
-            selection_sort(None)
-            selection_sort('1,2,3')
+        self._test_sort(selection_sort)
 
-        for test_case in TESTCASES:
-            self.assertEqual(selection_sort(test_case), sorted(test_case))
-    
     def test_insertion_sort(self):
+        self._test_sort(insertion_sort)
+
+    def test_shell_sort(self):
+        self._test_sort(shell_sort)
+
+    def test_merge_sort(self):
+        self._test_sort(merge_sort)
+    
+    def test_merge_sort_iteration(self):
+        self._test_sort(merge_sort_iteration)
+    
+    def test_quick_sort(self):
+        self._test_sort(quick_sort)
+
+    def _test_sort(self, sort_func):
         with self.assertRaises(ValueError):
-            insertion_sort(None)
-            insertion_sort('1,2,3')
+            sort_func(None)
+            sort_func('1,2,3')
 
         for test_case in TESTCASES:
-            self.assertEqual(insertion_sort(test_case), sorted(test_case))
+            self.assertEqual(sort_func(test_case), sorted(test_case))
 
 if __name__ == '__main__':
     unittest.main()
